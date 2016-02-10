483934e617c5e56194f0ff837d7c8a0f276a40c8
Gary Hastert <ghastert@stu.jsu.edu>
2016-02-10 12:23:30 -0600

    Merging

--- runacceptance.bat
+++ runacceptance.bat
@@ -1,6 +1,6 @@
 cd acceptance
 
 javac -cp .;..\build\classes\main ArgParserKeywords.java
-java -cp .;..\build\classes\main;C:\RobotFramework\robotframework-2.9.jar org.robotframework.RobotFramework ArgParserTest.txt
+java -cp .;..\build\classes\main;C:\Users\Owner\Desktop\CS310\robotframework-2.9.jar org.robotframework.RobotFramework ArgParserTest.txt
 cd ..
 pause
