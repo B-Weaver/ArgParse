cb383ad237f230e4496246cec9e6ff7c41225bdb
Gary Hastert <ghastert@stu.jsu.edu>
2016-03-03 15:10:03 -0600

    Added demo for new features. New class in demos called VolumeCalculatorB.java. Still use compileAndRunDemo.bat.

--- compileAndRunDemo.bat
+++ compileAndRunDemo.bat
@@ -22,3 +22,34 @@ cls
 
 java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculator -h
 pause
+
+javac -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB.java
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 --digits 1 --type ellipsoid
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 --digits 1
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 --help
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 -d 1 -t ellipsoid
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 -t ellipsoid
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorB 7 5 2 -d 5
+pause
---
+++ demos/VolumeCalculatorB.java
@@ -0,0 +1,25 @@
+import edu.jsu.mcis.*;
+
+public class VolumeCalculatorB{
+	public static void main(String[] args) {
+		ArgumentParser parser = new ArgumentParser();
+		parser.addArg("length","the length of the box");
+		parser.addArg("width", "the width of the box");
+		parser.addArg("height", "the height of the box");
+		parser.addArg("type");
+		parser.addArg("digits");
+		try {
+			parser.checkArgsThenParse(args);
+			int l = Integer.parseInt(parser.getArg("length"));
+			int w = Integer.parseInt(parser.getArg("width"));
+			int h = Integer.parseInt(parser.getArg("height"));
+			int v = l*w*h;
+			System.out.println("The volume is: " + v);
+			System.out.println("Digits: " +parser.getArg("digits"));
+			System.out.println("Type: " +parser.getArg("type"));
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+    }
+}
