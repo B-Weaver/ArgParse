59529523e6618933d8f0be875194a9b874d00f4b
Gary Hastert <ghastert@stu.jsu.edu>
2016-03-18 10:33:30 -0500

    Figured out how to make gradle generate Javadoc. Need to finish making the docs themselves.

--- onepressb.bat
+++ onepressb.bat
@@ -1,3 +1,3 @@
-call gradle clean build jacocoTestReport
+call gradle clean build jacocoTestReport javadoc
 explorer "file:///%~dp0build/reports/tests/index.html"
 explorer "file:///%~dp0build/reports/jacoco/test/html/index.html"
