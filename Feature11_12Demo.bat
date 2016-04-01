f434e6132de0978d755cf58327d3631b60fa6608
Barry Weaver <barry.e.weaver3@gmail.com>
2016-04-01 11:13:40 -0500

    batch file updated

--- Feature11_12Demo.bat
+++ Feature11_12Demo.bat
@@ -10,3 +10,12 @@ cls
 
 java -cp .;..\build\libs\ArgParse-1.0.jar VolumeCalculatorD 7 5 2 --type ellipsoid -d 8
 pause
+cls
+
+javac -cp .;..\build\libs\ArgParse-1.0.jar PizzaBuilder.java
+pause
+cls
+
+java -cp .;..\build\libs\ArgParse-1.0.jar PizzaBuilder 18 2 4 --cheese motzorella -d hand-tossed
+pause
+cls
