9284648e475c2be3f500115d253df1b14d15b830
Barry Weaver <barry.e.weaver3@gmail.com>
2016-04-13 11:58:14 -0500

    javadoc work

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -42,25 +42,25 @@ import org.xml.sax.SAXParseException;
 *<blockquote><pre>
 *{@code
 *public class VolumeCalculatorD{
-*public static void main(String[] args) {
-*	String filename = "C:/Users/Owner/Desktop/CS310/ArgParse/ArgParse/ArgParse/demos/Feature12Ex.xml";
-*	try{
-*		ArgumentParser parser = XMLTools.load(filename);
-*		parser.parseArgs(args);
-*		int l = Integer.parseInt(parser.getArg("length"));
-*		int w = Integer.parseInt(parser.getArg("width"));
-*		int h = Integer.parseInt(parser.getArg("height"));
-*		int v = l*w*h;
-*		System.out.println("The volume is: " + v);
-*		System.out.println("Digits: " +parser.getArg("digits"));
-*		System.out.println("Type: " +parser.getArg("type"));
+*	public static void main(String[] args) {
+*		String filename = "C:/Users/Owner/Desktop/CS310/ArgParse/ArgParse/ArgParse/demos/Feature12Ex.xml";
+*		try{
+*			ArgumentParser parser = XMLTools.load(filename);
+*			parser.parseArgs(args);
+*			int l = Integer.parseInt(parser.getArg("length"));
+*			int w = Integer.parseInt(parser.getArg("width"));
+*			int h = Integer.parseInt(parser.getArg("height"));
+*			int v = l*w*h;
+*			System.out.println("The volume is: " + v);
+*			System.out.println("Digits: " +parser.getArg("digits"));
+*			System.out.println("Type: " +parser.getArg("type"));
 *		
-*		XMLTools.save(parser, "C:/Users/Owner/Desktop/CS310/ArgParse/ArgParse/ArgParse/demos/Feature12Out.xml");
-*	}
-*	catch(Exception e) {
-*		System.out.println(e.getMessage());
-*	}
-* }
+*			XMLTools.save(parser, "C:/Users/Owner/Desktop/CS310/ArgParse/ArgParse/ArgParse/demos/Feature12Out.xml");
+*		}
+*		catch(Exception e) {
+*			System.out.println(e.getMessage());
+*		}
+* 	}
 * }
 *}
 *</pre></blockquote>
